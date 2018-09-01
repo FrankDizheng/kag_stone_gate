c4e0aaf8d155150786cafddd79ad62b9bf70e459
Dizheng Wang <dizhengw@usc.edu>
2018-09-01 21:06:22 +0800

    Create TextPaper.py
    This is for memorizing

---
+++ week1/TextPaper.py
@@ -0,0 +1,36 @@
+# seaborn import and color_palette() setting
+
+# get rid of warnings
+import warnings
+def ignore_warn(*args, **kwargs):
+  pass
+warnings.warn = ignore_warn
+
+# import plot object from matplotlib
+
+# scatter plot
+def scatterplot(df_train):
+  pass
+# norm & skew import from scipy
+
+# two ways to check data files in working directory
+
+# distribution plot using seaborn
+
+# probability plot
+
+# Generate dataframe to display missing value ratio
+def missing_value(df_train):
+  pass
+  
+# Plot missing value ratio using seaborn bar plot
+
+# Data correlation heat map using seaborn heatmap
+
+# root-mean-squared-error cross validation and its relative import
+def rmse_cv(model):
+  pass
+  
+# Average stacking model
+
+# Advanced stacking model
